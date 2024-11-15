70d7a2886c74696c8ed98c08489818ab57380e36
Zedd pai <weichih.pai@imemrsive.com>
2024-11-15 18:56:27 +0800

    Add yml

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import map from "./map.jpg";
-import work from "./work.jpg";
-import teach from "./teach.jpg";
+import map from "./image/map.jpg";
+import work from "./image/work.jpg";
+import teach from "./image/teach.jpg";
+import logo from "./image/logo.png";
+import teacher1 from "./image/teacher1.jpg";
+import teacher2 from "./image/teacher2.jpg";
 import "./App.css";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 import { useState } from "react";
@@ -37,22 +40,36 @@ function App() {
   return (
     <div className="App font-serif">
       <header className="fixed top-0 left-0 right-0 bg-white shadow-md z-50">
-        <div className="mx-auto px-4 py-4 flex justify-between items-center">
-          Logo
+        <div
+          className="mx-auto px-1 py-1 flex justify-between items-center"
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img className="App-logo" src={logo} alt="logo" />
         </div>
       </header>
       <div className="Content pt-14">
         <div className="background-container flex flex-col items-center w-full ">
           <div className="text-content w-full py-48">
-            <p className="text-xl md:text-3xl py-4 font-semibold">吉客 GEEK</p>
-            <p className="text-xl md:text-4xl font-extrabold py-4">
-              FAANG講師手把手帶領系統化學習
+            <p className="text-xl md:text-3xl py-4 font-semibold">吉客學院</p>
+            <p className="text-xl md:text-3xl font-extrabold py-4">
+              FAANG講師 手把手系統化教學
+            </p>
+            <p className="text-xl md:text-2xl py-4 font-semibold">
+              軟體工程師 x 職涯規劃 x 提升競爭力
             </p>
             <p className="text-xl md:text-2xl py-4 font-semibold">
               針對想進入一線外商的工程師
             </p>
-            <button className="button mt-4 rounded-2xl shadow-md max-w-min font-bold whitespace-nowrap text-3xl">
-              預約免費技術諮詢
+            <button
+              className="button mt-4 rounded-2xl shadow-md max-w-min font-bold whitespace-nowrap text-2xl"
+              onClick={(event) => {
+                event.preventDefault();
+                window.open("https://page.line.me/428navbh");
+              }}
+            >
+              預約免費諮詢
             </button>
           </div>
         </div>
@@ -92,7 +109,7 @@ function App() {
         </div>
         <div className="py-20">
           <div className="font-serif text-4xl pb-20 font-bold">
-            SRI Solution
+            SRI 學習計劃
           </div>
           <div className="flex flex-col md:flex-row gap-10 items-center justify-center">
             <div className="max-w-2xl ">
@@ -132,7 +149,7 @@ function App() {
                   透過系統化教學與專屬任務系統
                 </p>
                 <p className="text-xl  font-semibold max-w-3xl pt-4">
-                  你會知道你該學什麼不該學什麼，專注在什麼樣的事情上才是重點
+                  你會知道你該學什麼，專注在正確的事情上才是重點
                 </p>
                 <p className="text-xl  font-semibold max-w-3xl pt-4">
                   職涯規劃 FAANG招聘形勢 簡歷準備 BQ準備 算法準備 面試準備+技巧
@@ -176,7 +193,7 @@ function App() {
           <p className="font-serif text-4xl py-4 font-bold">講師介紹</p>
           <div className="flex flex-col md:flex-row gap-10">
             <div className="max-w-2xl introduce">
-              <img className="rounded-3xl" src={map} alt="map" />
+              <img className="rounded-3xl" src={teacher1} alt="teacher1" />
               <div className="min-height-[20rem] p-4">
                 <div className="text-4xl name py-3">
                   <p>Zedd</p>
@@ -186,7 +203,7 @@ function App() {
                     資深全端工程師
                   </p>
                   <p className="  font-semibold max-w-3xl pt-1">
-                    在科技業大裁員時面試上FAANG
+                    在科技業大裁員時面試上Amazon Yahoo
                   </p>
                   <p className="  font-semibold max-w-3xl pt-1">
                     深入淺出講解面試必問考題，短時間內幫助學員上手
@@ -198,7 +215,7 @@ function App() {
               </div>
             </div>
             <div className="max-w-2xl introduce">
-              <img className="rounded-3xl" src={map} alt="map" />
+              <img className="rounded-3xl" src={teacher2} alt="teacher2" />
               <div className="min-height-[20rem] p-4">
                 <div className=" text-4xl name py-3">
                   <p>Kao</p>
@@ -214,7 +231,7 @@ function App() {
                     拿到頂尖美國offer10+，刷題1000+ 精通各類算法題型
                   </p>
                   <p className="  font-semibold max-w-3xl pt-1">
-                    豐富的面試和麵試輔導經驗，提供簡潔易懂並且有效的針對性輔導
+                    豐富的面試和面試輔導經驗，提供簡潔易懂並且有效的針對性輔導
                   </p>
                 </div>
               </div>
@@ -231,8 +248,8 @@ function App() {
           content="會有專屬學員頻道，學員能透過頻道與FAANG講師直接進行溝通。並且每週會定期舉辦問題QA諮詢，在線上一對一針對你的問題去做回覆"
         />
         <Collapse
-          title="要如何幫助我進入一線外商？"
-          content="專屬的任務系統以及系統化教學，讓你在學習的道路上有規劃與方向，幫助你有系統的學習，不浪費任何時間在不必要的事情上"
+          title="要如何幫助我進入一線外商大廠？"
+          content="專屬的任務系統以及系統化教學，讓你在學習的道路上有規劃與方向，幫助你有系統的學習。並且會有面試陪跑服務，透過來自大廠講師豐富的面試經驗幫助你拿到Offer"
         />
         <Collapse
           title="課程內容是什麼？"
@@ -242,16 +259,80 @@ function App() {
           title="提供什麼就業相關的幫助嗎？"
           content="提供模擬面試 履歷健檢 面試安排 面試技巧(演算法 系統設計 BQ 薪資 等等) 面試陪跑"
         />
-        <iframe
-          src="https://fang81.ck.page/4b8f49b1b9"
-          name="iframe_a"
-          height="450px"
-          width="100%"
-          title="Iframe Example"
-          scrolling="no"
-        ></iframe>
 
-        <div className="container pt-20 pb-10">
+        <div className="w-full">
+          <div className="w-full py-32 flex flex-col items-center">
+            <p className="text-xl md:text-2xl font-extrabold pt-5">
+              針對想進入一線外商大廠的工程師
+            </p>
+            <ul className="list-disc text-left text-xl md:text-2xl font-semibold">
+              <li className="pt-5">系統教學</li>
+              <li className="pt-5">任務系統</li>
+              <li className="pt-5">短期衝刺</li>
+              <li className="pt-5">模擬面試</li>
+              <li className="pt-5">面試陪跑</li>
+              <li className="pt-5">內推分享</li>
+            </ul>
+            <button
+              className="button mt-16 rounded-2xl shadow-md max-w-min font-bold whitespace-nowrap text-2xl"
+              onClick={(event) => {
+                event.preventDefault();
+                window.open("https://page.line.me/428navbh");
+              }}
+            >
+              預約免費諮詢
+            </button>
+          </div>
+        </div>
+        <div className="fixed bottom-8 left-8">
+          <a
+            href="https://page.line.me/428navbh"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>
+              <svg width="39" height="39" viewBox="0 0 39 39" fill="none">
+                <circle
+                  className="color-element"
+                  cx="19.4395"
+                  cy="19.4395"
+                  r="19.4395"
+                  fill="#38B900"
+                ></circle>
+                <path
+                  d="M24 9.36561C24 4.19474 18.6178 0 12 0C5.38215 0 0 4.19474 0 9.36561C0 13.9825 4.25629 17.8606 10.0229 18.5993C10.4073 18.6785 10.9565 18.8368 11.0664 19.1797C11.1762 19.4699 11.1487 19.9184 11.0938 20.235C11.0938 20.235 10.9565 21.0528 10.9291 21.2111C10.8741 21.5013 10.6819 22.3456 11.9725 21.8443C13.2632 21.3167 18.8924 17.9398 21.3913 15.1433C23.1487 13.2702 24 11.4234 24 9.36561Z"
+                  transform="translate(7 10)"
+                  fill="white"
+                ></path>
+                <path
+                  d="M1.0984 0H0.24714C0.10984 0 -2.09503e-07 0.105528 -2.09503e-07 0.211056V5.22364C-2.09503e-07 5.35555 0.10984 5.43469 0.24714 5.43469H1.0984C1.2357 5.43469 1.34554 5.32917 1.34554 5.22364V0.211056C1.34554 0.105528 1.2357 0 1.0984 0Z"
+                  transform="translate(15.4577 16.8593)"
+                  fill="#38B900"
+                  className="color-element"
+                ></path>
+                <path
+                  d="M4.66819 0H3.81693C3.67963 0 3.56979 0.105528 3.56979 0.211056V3.19222L1.18078 0.0791458C1.18078 0.0791458 1.18078 0.0527642 1.15332 0.0527642C1.15332 0.0527642 1.15332 0.0527641 1.12586 0.0263821C1.12586 0.0263821 1.12586 0.0263821 1.0984 0.0263821H0.247139C0.10984 0.0263821 4.19006e-07 0.13191 4.19006e-07 0.237438V5.25002C4.19006e-07 5.38193 0.10984 5.46108 0.247139 5.46108H1.0984C1.2357 5.46108 1.34554 5.35555 1.34554 5.25002V2.26885L3.73455 5.38193C3.76201 5.40831 3.76201 5.43469 3.78947 5.43469C3.78947 5.43469 3.78947 5.43469 3.81693 5.43469C3.81693 5.43469 3.81693 5.43469 3.84439 5.43469C3.87185 5.43469 3.87185 5.43469 3.89931 5.43469H4.75057C4.88787 5.43469 4.99771 5.32917 4.99771 5.22364V0.211056C4.91533 0.105528 4.80549 0 4.66819 0Z"
+                  transform="translate(17.6819 16.8593)"
+                  fill="#38B900"
+                  className="color-element"
+                ></path>
+                <path
+                  d="M3.62471 4.22112H1.34554V0.237438C1.34554 0.105528 1.2357 0 1.0984 0H0.24714C0.10984 0 -5.23757e-08 0.105528 -5.23757e-08 0.237438V5.25002C-5.23757e-08 5.30278 0.0274599 5.35555 0.0549198 5.40831C0.10984 5.43469 0.16476 5.46108 0.21968 5.46108H3.56979C3.70709 5.46108 3.78947 5.35555 3.78947 5.22364V4.4058C3.87185 4.32665 3.76201 4.22112 3.62471 4.22112Z"
+                  transform="translate(10.8993 16.8593)"
+                  fill="#38B900"
+                  className="color-element"
+                ></path>
+                <path
+                  d="M3.56979 1.29272C3.70709 1.29272 3.78947 1.18719 3.78947 1.05528V0.237438C3.78947 0.105528 3.67963 -1.00639e-07 3.56979 -1.00639e-07H0.219679C0.164759 -1.00639e-07 0.10984 0.0263821 0.0549199 0.0527641C0.02746 0.105528 -2.09503e-07 0.158292 -2.09503e-07 0.211056V5.22364C-2.09503e-07 5.2764 0.02746 5.32917 0.0549199 5.38193C0.10984 5.40831 0.164759 5.43469 0.219679 5.43469H3.56979C3.70709 5.43469 3.78947 5.32917 3.78947 5.19726V4.37941C3.78947 4.2475 3.67963 4.14198 3.56979 4.14198H1.29062V3.29775H3.56979C3.70709 3.29775 3.78947 3.19222 3.78947 3.06031V2.24247C3.78947 2.11056 3.67963 2.00503 3.56979 2.00503H1.29062V1.16081H3.56979V1.29272Z"
+                  transform="translate(23.421 16.8329)"
+                  fill="#38B900"
+                  className="color-element"
+                ></path>
+              </svg>
+            </span>
+          </a>
+        </div>
+        <div className="container pb-10">
           <div className="text-lg">
             <p>Gike-Geek © 2024 All Rights Reserved</p>
           </div>
